14766c7320ed49b106b22a421d52e3215be3f3d9
LiYuanyuan0523 <126186043+LiYuanyuan0523@users.noreply.github.com>
2023-11-01 22:25:32 +0800

    Update user.sql

--- user.sql
+++ user.sql
@@ -2,11 +2,10 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS USER
-{
+(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_email varchar(50) NOT NULL
-    user_password varchar(20) NOT NULL
-}
+    user_email varchar(50) NOT NULL,
+    user_password varchar(20) NOT NULL);
 
 INSERT INTO USER(id, user_email, user_password) VALUES(1, 'Alice111@hotmail.com', '111123456');
 INSERT INTO USER(id, user_email, user_password) VALUES(2, 'Brian222@gmail.com', '222123456');
