3d45279257673a5f8d262e25dfa121b47f9de257
Zhu Bangyuan <46257201+bangyuanzhu@users.noreply.github.com>
2023-11-01 14:57:05 +0800

    Create user.sql

---
+++ user.sql
@@ -0,0 +1,16 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE IF NOT EXISTS USER
+{
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id varchar(20) NOT NULL
+    user_email varchar(50) NOT NULL
+    user_password varchar(20) NOT NULL
+}
+
+INSERT INTO USER(id, user_id, user_email, user_password) VALUES(1, 'sam heng', 'cronous999@hotmail.com', '96462991')
+INSERT INTO USER(id, user_id, user_email, user_password) VALUES(2, 'Heng Seng En', 'skyscraper960@gmail.com', '96462994')
+INSERT INTO USER(id, user_id, user_email, user_password) VALUES(3, 'kenji', 'kenji@gmail.com', '12345678')
+INSERT INTO USER(id, user_id, user_email, user_password) VALUES(4, 'karin tan', 'karintan@hotmail.com', '87654321')
+INSERT INTO USER(id, user_id, user_email, user_password) VALUES(5, 'kenji', 'sengen96@gmail.com', '12345678')
