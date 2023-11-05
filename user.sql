b93f74b82549562aec283106650dc1e6c2de93f3
LiYuanyuan0523 <126186043+LiYuanyuan0523@users.noreply.github.com>
2023-11-06 01:54:34 +0800

    Update user.sql

--- user.sql
+++ user.sql
@@ -5,7 +5,8 @@ CREATE TABLE IF NOT EXISTS USER
 (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_email varchar(50) NOT NULL,
-    user_password varchar(20) NOT NULL);
+    user_password varchar(20) NOT NULL
+);
 
 INSERT INTO USER(id, user_email, user_password) VALUES(1, 'Alice111@hotmail.com', '111123456');
 INSERT INTO USER(id, user_email, user_password) VALUES(2, 'Brian222@gmail.com', '222123456');
